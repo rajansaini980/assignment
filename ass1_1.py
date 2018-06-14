9a19b0c22cde8da7e9daa4225587f4add864b4f8
rajansaini980 <rajansaini980@gmail.com>
2018-06-14 03:24:45 -0700

    files are added

---
+++ ass1_1.py
@@ -0,0 +1,18 @@
+print("my name is rajan saini")
+
+x="acad"
+y="view"
+z=x+y
+print(z)
+
+x=5
+y=2.5
+z="a"
+print(x,y,z)
+
+print('"lets get started"')
+
+s="acadview"
+course="pyhton"
+fees=5000
+print("s,course,fees")
---
+++ ass2_1.py
@@ -0,0 +1,45 @@
+x=int(input("enter any number"))
+y=int(input("enter any number"))
+z=int(input("enter any number"))
+l=[x,y,z]
+l1=["google","apple","facebook","microsoft","tesla"]
+print(l.extend(l1))
+print(l)
+
+
+x=[1,2,3,4,5,6,7]
+print( x.count(3))
+
+
+l1=[1,4,3]
+print(l1)
+l1.sort()
+print(l1)
+l2=[3,2,1]
+print(12)
+l2.sort()
+print(l2)
+l3=l1+l2
+print(l3)
+
+
+a=[5,1,3]
+b=[5,4,7]
+a.sort()
+b.sort()
+c=a+b
+print(c)
+
+
+l=[]
+l.append(int(input("enter any number")))
+l.append(int(input("enter any number")))
+l.append(int(input("enter any number")))
+print(l)
+l.pop()
+print(l)
+l.append(int(input("enter any number")))
+l.append(int(input("enter any number")))
+print(l)
+l.pop()
+print(l)
